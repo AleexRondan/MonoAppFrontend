f9a3cedfe91e9dc9bd5c89830b479420fab1ef98
AleexRondan <rondangarciaa@gmail.com>
2025-11-28 19:48:04 +0200

    changed localhost for webpage

--- homePage/src/App.jsx
+++ homePage/src/App.jsx
@@ -117,7 +117,7 @@ function HomePage() {
       }
 
       window.location.href =
-        "http://localhost:5173/?token=" + encodeURIComponent(token);
+        "https://mono-app-inpage.vercel.app" + encodeURIComponent(token);
     } catch (err) {
       console.error("Login error:", err);
       setLoginError("Network error. Please try again.");
